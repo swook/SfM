f1fab7c531da37f28ac09eebbe47e5c00153eaeb
Seon-Wook Park <seon.wook@swook.net>
2015-03-26 16:48:20 +0100

    Pull vlfeat in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,23 @@ DATA_DIR=data/
 # Cleanup
 # rm -rf $DATA_DIR
 
+## BEGIN 3rd party libs
+if [ ! -d lib/ ]; then
+	mkdir lib/
+fi
+cd lib/
+
+# Get vlfeat
+if [ ! -d vlfeat/ ]; then
+	git clone git@github.com:vlfeat/vlfeat.git
+fi
+cd vlfeat
+git pull origin master
+cd ..
+
+cd ..
+## END 3rd party
+
 # Get datasets
 wget -Nnv http://files.swook.net/sfm_data.zip
 unzip -oqq sfm_data.zip
