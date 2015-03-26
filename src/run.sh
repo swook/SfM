35f29f6c6bf7999309e95e8300ab7314ccef1555
Seon-Wook Park <seon.wook@swook.net>
2015-03-26 20:45:12 +0100

    Add Darwin-specific changes for run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-make && LD_LIBRARY_PATH=$(dirname ../lib/**/bin/**/libvl.so) ./main ../data
+# Compile
+make
+
+OS=$(uname -s)
+
+# Run
+if [ "$OS" = "Darwin" ]; then
+	DYLD_LIBRARY_PATH=$(dirname ../lib/**/bin/**/libvl.dyld) ./main ../data
+elif [ "$OS" = "Linux" ]; then
+	LD_LIBRARY_PATH=$(dirname ../lib/**/bin/**/libvl.so) ./main ../data
+fi
