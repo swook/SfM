2fc9e15fcf74d866d4d253431f0cb686bf94dda2
Seon-Wook Park <seon.wook@swook.net>
2015-04-01 16:21:09 +0200

    Add ceres and opencv to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,18 +6,53 @@ DATA_DIR=data/
 # rm -rf $DATA_DIR
 
 ## BEGIN 3rd party libs
+LIBDIR="$(dirname $(pwd))/lib/"
 if [ ! -d lib/ ]; then
 	mkdir lib/
 fi
 cd lib/
 
 # Get vlfeat
-if [ ! -d vlfeat/ ]; then
+VLDIR="$LIBDIR/vlfeat/"
+if [ ! -d "$VLDIR" ]; then
 	git clone https://github.com/vlfeat/vlfeat.git
 fi
-cd vlfeat
+cd "$VLDIR"
 git pull origin master
 make
+cd ..
+
+# Get OpenCV 3
+CVDIR="$LIBDIR/opencv/"
+if [ ! -d "$CVDIR" ]; then
+	git clone https://github.com/Itseez/opencv.git
+	git clone https://github.com/Itseez/opencv_contrib.git
+fi
+cd $CVDIR
+git pull origin master
+# START OPENCV BUILD
+	rm -rf build/
+	mkdir build/
+	OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules cmake ..
+	make -j$(nproc)
+	cd ..
+# END OPENCV BUILD
+cd ..
+
+# Get ceres
+CERESDIR="$LIBDIR/ceres-solver/"
+if [ ! -d "$CERESDIR" ]; then
+	git clone https://ceres-solver.googlesource.com/ceres-solver
+fi
+cd "$CERESDIR"
+git pull origin master
+# START CERES BUILD
+	rm -rf build/
+	mkdir build/
+	cmake -D CMAKE_BUILD_TYPE=RELEASE WITH_TBB=ON ..
+	make -j$(nproc)
+	cd ..
+# END CERES BUILD
 cd ..
 
 cd ..
