fe229c7bc476c5f606153347d5257654c0e3427e
Seon-Wook Park <seon.wook@swook.net>
2015-04-12 14:38:46 +0200

    Add custom logger

--- src/util.hpp
+++ src/util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <cstdarg>
 
 #include "opencv2/opencv.hpp"
 using namespace cv;
@@ -20,4 +21,32 @@ void showImageAndWait(const char* title, const Mat& img)
 	std::cout << "Press any key to continue..." << std::endl;
 	waitKey(0);
 }
+
+/**
+ * Custom logger, instantiate with namespace string to prefix messages with.
+ * For example:
+ *     Logger _log("Load Images");
+ */
+class Logger {
+private:
+	const char* name_space;
+
+public:
+	Logger(const char* _namespace)
+	{
+		name_space = _namespace;
+	}
+
+	void operator() (const char* format, ...)
+	{
+		va_list args;
+		va_start(args, format);
+
+		printf("%s> ", name_space);
+		vprintf(format, args);
+		printf("\n");
+
+		va_end(args);
+	}
+};
 
