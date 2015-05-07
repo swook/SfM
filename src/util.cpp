73ea000649ac6b654d4964f62ef971024dcbc6ec
yifita <yifanwang2010@gmail.com>
2015-05-07 22:53:34 +0200

    Rotation2quaternion

--- src/util.cpp
+++ src/util.cpp
@@ -1,6 +1,6 @@
 #include <cmath>
 #include <cstdarg>
-
+#include <assert.h>
 #include "opencv2/opencv.hpp"
 using namespace cv;
 
@@ -47,4 +47,55 @@ void Logger::operator() (const char* format, ...)
 
 	va_end(args);
 }
+/*
+ * convert rotation matrix to quaternion
+*/
+Vec4f R2Quaternion(Mat& R)
+{
+	assert(R.rows == 3 && R.cols == 3);
+	// typedef typename DataType<float>::work_type _wTp;
+	if (R.type() != CV_32F) R.convertTo(R, CV_32F);;
+	float q0,q1,q2,q3;
+	float r00 = R.at<float>(1,1);
+	float r01 = R.at<float>(1,2);
+	float r02 = R.at<float>(1,3);
+	float r10 = R.at<float>(2,1);
+	float r11 = R.at<float>(2,2);
+	float r12 = R.at<float>(2,3);
+	float r20 = R.at<float>(3,1);
+	float r21 = R.at<float>(3,2);
+	float r22 = R.at<float>(3,3);
+
+	float trace = r00 + r11 + r22;
+	if( trace > 0 ){
+		float s = 0.5 / sqrt(trace+ 1.0);
+    	q0 = 0.25 / s;
+    	q1 = ( r21 - r12 ) * s;
+    	q2 = ( r02 - r20 ) * s;
+    	q3 = ( r10 - r01 ) * s;
+	} else {
+    	if ( r00 > r11 && r00 > r22 ) 
+    	{
+      	float s = 2.0 * sqrt( 1.0 + r00 - r11 - r22);
+      	q0 = (r21 - r12 ) / s;
+      	q1 = 0.25 * s;
+      	q2 = (r01 + r10 ) / s;
+      	q3 = (r02 + r20 ) / s;
+    	} else if (r11 > r22) {
+	    float s = 2.0 * sqrt( 1.0 + r11 - r00 - r22);
+	    q0 = (r02 - r20 ) / s;
+	    q1 = (r01 + r10 ) / s;
+	    q2 = 0.25 * s;
+	    q3 = (r12 + r21 ) / s;
+    	} else {
+      	float s = 2.0 * sqrt( 1.0 + r22 - r00 - r11 );
+      	q0 = (r10 - r01 ) / s;
+	    q1 = (r02 + r20 ) / s;
+	    q2 = (r12 + r21 ) / s;
+	    q3 = 0.25 * s;
+    	}
+	}
+	Vec4f q = normalize(Vec<float,4>::Vec(q0, q1, q2, q3));
+}
+
 
--- src/util.hpp
+++ src/util.hpp
@@ -10,7 +10,7 @@ typedef unsigned int uint;
 void showImage(const char* title, const cv::Mat& img);
 void showImageAndWait(const char* title, const cv::Mat& img);
 cv::Point3f backproject3D(const float x,const float y, float depth, const cv::Mat m_cameraMatrix);
-
+cv::Vec4f R2Quaternion(cv::Mat& R);
 /**
  * Custom logger, instantiate with namespace string to prefix messages with.
  * For example:
