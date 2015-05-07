8d1744f0c1488d420f812a78d5600be0d21c2fff
Seon-Wook Park <seon.wook@swook.net>
2015-05-07 22:47:59 +0200

    Use BRISK for speed

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -38,16 +38,22 @@ void Pipeline::run()
 	 */
 	ImagePairs image_pairs;
 	find_matching_pairs(images, cam_Frames, descriptors_vec, image_pairs);
-	// free (&images); or actually free everything in images except rgb
+
+
+	// Free some memory
+	Images().swap(images);
+	DescriptorsVec().swap(descriptors_vec);
+
+
 
 	/**
 	* State 3: Compute pairwise R and t
 	*/
-	register_camera(image_pairs,cam_Frames);
+	register_camera(image_pairs, cam_Frames);
 
 
 	/**
-	 * Stage 3.5: Construct associative matrix
+	 * Stage 3.5: Construct associativity matrix
 	 */
 	Associativity assocMat(cam_Frames.size());
 	for (int p = 0; p < image_pairs.size(); p++)
@@ -64,12 +70,13 @@ void Pipeline::run()
 	}
 
 
-
 	/**
 	 * Stage 4: Compute global Rs and ts
 	 */
 	CameraPoses gCameraPoses;
 	build_spanning_tree(gCameraPoses, image_pairs, assocMat);
 
+
+	// End
 	_log.tok();
 }
