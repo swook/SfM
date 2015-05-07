f133c617c254427ce14fc5d167aa317d8657d569
Seon-Wook Park <seon.wook@swook.net>
2015-05-07 16:42:33 +0200

    Split util.hpp to util.cpp

---
+++ src/util.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <cstdarg>
+
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+#include "util.hpp"
+
+void showImage(const char* title, const Mat& img)
+{
+	std::cout << "\nShowing image: \"" << title << "\"." << std::endl;
+	namedWindow(title, CV_WINDOW_NORMAL);
+	imshow(title, img);
+}
+
+void showImageAndWait(const char* title, const Mat& img)
+{
+	showImage(title, img);
+	std::cout << "Press any key to continue..." << std::endl;
+	waitKey(0);
+}
+
+Point3f backproject3D(const float x,const float y, float depth, const Mat m_cameraMatrix)
+{
+	Mat new_point = depth * (Mat_<float>(1,3) << x,y,1) * m_cameraMatrix.inv().t();
+	return Point3f(new_point);
+}
+
+/**
+ * Custom logger, instantiate with namespace string to prefix messages with.
+ * For example:
+ *     Logger _log("Load Images");
+ */
+Logger::Logger(const char* _namespace)
+{
+	name_space = _namespace;
+}
+
+void Logger::operator() (const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	printf("%s> ", name_space);
+	vprintf(format, args);
+	printf("\n");
+
+	va_end(args);
+}
+
--- src/util.hpp
+++ src/util.hpp
@@ -4,29 +4,12 @@
 #include <cstdarg>
 
 #include "opencv2/opencv.hpp"
-using namespace cv;
 
 typedef unsigned int uint;
 
-void showImage(const char* title, const Mat& img)
-{
-	std::cout << "\nShowing image: \"" << title << "\"." << std::endl;
-	namedWindow(title, CV_WINDOW_NORMAL);
-	imshow(title, img);
-}
-
-void showImageAndWait(const char* title, const Mat& img)
-{
-	showImage(title, img);
-	std::cout << "Press any key to continue..." << std::endl;
-	waitKey(0);
-}
-
-Point3f backproject3D(const float x,const float y, float depth, const Mat m_cameraMatrix)
-{
-	Mat new_point = depth * (Mat_<float>(1,3) << x,y,1) * m_cameraMatrix.inv().t();
-	return Point3f(new_point);
-}
+void showImage(const char* title, const Mat& img);
+void showImageAndWait(const char* title, const Mat& img);
+Point3f backproject3D(const float x,const float y, float depth, const Mat m_cameraMatrix);
 
 /**
  * Custom logger, instantiate with namespace string to prefix messages with.
@@ -38,21 +21,7 @@ private:
 	const char* name_space;
 
 public:
-	Logger(const char* _namespace)
-	{
-		name_space = _namespace;
-	}
-
-	void operator() (const char* format, ...)
-	{
-		va_list args;
-		va_start(args, format);
-
-		printf("%s> ", name_space);
-		vprintf(format, args);
-		printf("\n");
-
-		va_end(args);
-	}
+	Logger(const char* _namespace);
+	void operator() (const char* format, ...);
 };
 
