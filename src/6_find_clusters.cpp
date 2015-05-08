3bf4e9b5057ce0e8c677737f2fe717d5aa1bf01e
yifita <yifanwang2010@gmail.com>
2015-05-08 16:29:45 +0200

    starting 6_find_clusters

---
+++ src/6_find_clusters.cpp
@@ -0,0 +1,10 @@
+#include "opencv2/core.hpp"
+using namespace cv;
+
+#include "Pipeline.hpp"
+#include "Associativity.hpp"
+#include "util.hpp"
+
+void Pipeline::find_clusters(Associativity& asso){
+	
+}
